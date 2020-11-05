5f3bc16262db1ee770b5b99bd0b66b67cef64e6d
Дарья Козак <keetapy@gmail.com>
2020-11-05 15:15:15 +0200

    Добавьте файлы проекта.

---
+++ Lime.Business/Services/AuthorizationService.cs
@@ -0,0 +1,13 @@
+﻿using Lime.Business.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lime.Business.Services
+{
+    class AuthorizationService: IAuthorizationService
+    {
+    }
+}
---
+++ Lime.Business/Services/Interfaces/IAuthorizationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lime.Business.Services.Interfaces
+{
+    public interface IAuthorizationService
+    {
+    }
+}
