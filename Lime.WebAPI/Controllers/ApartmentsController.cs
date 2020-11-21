b0359c0bc8af5660e6be2c13ea9cdff4902ba821
Дарья Козак <keetapy@gmail.com>
2020-11-21 14:16:10 +0200

    Auth attribute

--- Lime.WebAPI/Controllers/ApartmentsController.cs
+++ Lime.WebAPI/Controllers/ApartmentsController.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
 using Lime.Business.Services.Interfaces;
 using Lime.ViewModels.ViewModels;
 using Lime.ViewModels.Views;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lime.WebAPI.Controllers
 {
     [Route("api/apartment")]
     [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class ApartmentsController : ControllerBase
     {
         private readonly IApartmentsService _apartmentsService;
