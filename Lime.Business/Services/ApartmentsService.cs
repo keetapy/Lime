a971e2ac2fc4a3706b5d9d36af50544ad6ef1622
Дарья Козак <keetapy@gmail.com>
2020-11-26 13:39:08 +0200

    Apartment fix

--- Lime.Business/Services/ApartmentsService.cs
+++ Lime.Business/Services/ApartmentsService.cs
@@ -15,9 +15,7 @@ namespace Lime.Business.Services
 {
     public class ApartmentsService : IApartmentsService
     {
-        //private readonly UsersDbContext _context;
         private readonly IApartmentRepository _apartmentsRepository;
-        //UsersDbContext context
         public ApartmentsService(IApartmentRepository repository)
         {
             _apartmentsRepository = repository;
@@ -40,42 +38,7 @@ namespace Lime.Business.Services
                     DealTypeId=apartment.DealTypeId
                 });
             }
-            #region GetWithContext
-            //List<GetApartmentView> apartments = new List<GetApartmentView>();
-            //foreach (var apartment in _context.Apartments.ToList())
-            //{
-            //    apartment.InternetProvider = _context.InternetProviders.FirstOrDefault(x => x.Id == apartment.InternetProviderId);
-            //    apartment.ApartmentAddress = _context.ApartmentAddresses.FirstOrDefault(x => x.Id == apartment.Id);
-            //    GetApartmentView newView = new GetApartmentView
-            //    {
-            //        Price = apartment.Price,
-            //        DealType = _context.DealTypes.FirstOrDefault(x => x.Id == apartment.DealTypeId).Name,
-            //        ApartmentAddress = new ApartmentAddressViewItem
-            //        {
-            //            PostalCode = apartment.ApartmentAddress.PostalCode,
-            //            City = apartment.ApartmentAddress.City,
-            //            Country = apartment.ApartmentAddress.Country,
-            //            Street = apartment.ApartmentAddress.Street,
-            //            HouseNumber = apartment.ApartmentAddress.HouseNumber
-            //        },
-            //        FlatNumber = apartment.FlatNumber,
-            //        ApartmentType = _context.ApartmentTypes.FirstOrDefault(x => x.Id == apartment.ApartmentTypeId).Name,
-            //        ApartmentSquare = apartment.ApartmentSquare,
-            //        InternetProvider = new InternetProviderViewItem
-            //        {
-            //            Id = apartment.InternetProvider.Id,
-            //            Name = apartment.InternetProvider.Name,
-            //            Phone = apartment.InternetProvider.Phone,
-            //            Email = apartment.InternetProvider.Email
-            //        }
-            //    };
-
-            //    apartments.Add(newView);
-            //}
-
-            #endregion
             return apartments;
-            //throw new NotImplementedException();
         }
     }
 }
