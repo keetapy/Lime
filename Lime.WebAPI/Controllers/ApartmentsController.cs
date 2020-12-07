034fb822fbd7116561b1758548970b99927e5149
Дарья Козак <keetapy@gmail.com>
2020-12-07 19:47:11 +0200

    First traceLog

--- Lime.WebAPI/Controllers/ApartmentsController.cs
+++ Lime.WebAPI/Controllers/ApartmentsController.cs
@@ -8,6 +8,7 @@ using Lime.ViewModels.Views;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Lime.WebAPI.Controllers
 {
@@ -17,24 +18,34 @@ namespace Lime.WebAPI.Controllers
     public class ApartmentsController : ControllerBase
     {
         private readonly IApartmentsService _apartmentsService;
-        public ApartmentsController(IApartmentsService apartmentsService)
+        private readonly ILogger<ApartmentsController> _logger;
+
+        public ApartmentsController(IApartmentsService apartmentsService, ILogger<ApartmentsController> logger)
         {
             _apartmentsService = apartmentsService;
+            _logger = logger;
+            _logger.LogInformation("ApartmentsController is created");
+
         }
         [HttpGet("get")]
         public async Task<List<GetApartmentDapperView>> GetApartments()
         {
+            _logger.LogInformation("GetApartments started");
+
             return await _apartmentsService.GetApartments();
         }
         [HttpGet("getbyid/{id:int}")]
         public async Task<GetApartmentView> GetApartmentsById(int id)
         {
-           
+            _logger.LogInformation("GetApartment with id: "+id);
+
             return await _apartmentsService.GetApartmentById(id);
         }
         [HttpPost("set")]
         public async Task<List<GetApartmentDapperView>> SetApartments(List<SetApartmentsViewModel> viewModels)
         {
+            _logger.LogInformation("SetApartments started");
+
             return await _apartmentsService.SetApartments(viewModels);
         }
     }
--- Lime.WebAPI/Controllers/AuthorizationController.cs
+++ Lime.WebAPI/Controllers/AuthorizationController.cs
@@ -3,6 +3,7 @@ using Lime.Business.Services.Interfaces;
 using Lime.ViewModels.ViewModels;
 using Lime.ViewModels.Views;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Lime.WebAPI.Controllers
 {
@@ -11,13 +12,18 @@ namespace Lime.WebAPI.Controllers
     public class AuthorizationController : ControllerBase
     {
         private readonly IAuthorizationService _authorizationService;
-        public AuthorizationController(IAuthorizationService authorizationService)
+        private readonly ILogger<AuthorizationController> _logger;
+        public AuthorizationController(IAuthorizationService authorizationService, ILogger<AuthorizationController> logger)
         {
             _authorizationService = authorizationService;
+            _logger = logger;
+            _logger.LogInformation("AuthorizationController is created");
         }
         [HttpPost("singin")]
         public async Task<SignInAccountView> SignIn([FromBody] SignInAuthorizationViewModel model)
         {
+            _logger.LogInformation("SignIn by "+model.Email );
+
             //test values for viewModel in postman
             //var result= await _authorizationService.SignIn(new SignInAuthorizationViewModel { Email = $"kjjbjk@gmail.com", Password = "!3422k34P56" });
             return await _authorizationService.SignIn(model);
@@ -25,6 +31,7 @@ namespace Lime.WebAPI.Controllers
         [HttpPost("singup")]
         public async Task<string> SingUp([FromBody]SignUpAuthorizationViewModel viewModel)
         {
+            _logger.LogInformation("SingUp by " + viewModel.Email);
             //test values for viewModel in postman
             //var viewModel = new SignUpAuthorizationViewModel { UserName = $"qer@gmail.com", Email = $"spofkpsoe@gmail.com", Password = "!3422k34P56" };
             var result=await _authorizationService.SingUp(viewModel);
